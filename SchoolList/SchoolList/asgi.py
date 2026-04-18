54f4c43e434e8b14d51b543ac73719381e38ce8c
Erastus Mwaniki <mwanikierastus32@gmail.com>
2026-04-18 16:42:01 +0000

    The initial program structure

---
+++ SchoolList/SchoolList/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SchoolList project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SchoolList.settings')
+
+application = get_asgi_application()
